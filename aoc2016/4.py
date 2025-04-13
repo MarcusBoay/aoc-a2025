2c32597977033a05dc383e2b3b216334a6184fe4
MarcusBoay <marcusboay@hotmail.com>
2025-04-13 13:38:11 -0400

    2016 4.1

--- aoc2016/4.py
+++ aoc2016/4.py
@@ -16,21 +16,59 @@ class Solution:
         fp = open(fileName, 'r')
         arr = list(map(str, fp.read().split('\n')))
         for i in range(0, len(arr)):
-            line = arr[i].split('|')
-            self.ins.append(line[::])
+            self.ins.append(arr[i][:])
         fp.close()
         print("===", self.fileName, "===")
 
     def solve1(self):
         print("--- Part One ---")
-        matches = re.findall(r"mul\((\d+),(\d+)\)", self.ins)
-        result = 0
-        for match in matches:
-            result += int(match[0]) * int(match[1])
-        print("Result:", result)
-        dontIter = re.finditer(r"don't\(\)", self.ins)
-        doIter = re.finditer(r"do\(\)", self.ins)
-        mulIter = re.finditer(r"mul\((\d+),(\d+)\)", self.ins)
+        sumOfSectorIDs = 0
+        for room in self.ins:
+            # get checksum
+            checksum = set()
+            for i in range(len(room)-6, len(room)-1):
+                checksum.add(room[i])
+
+            # get sector id
+            room = room[0:len(room)-7].split('-')
+            sectorID = int(room.pop())
+
+            # get count of all letters
+            letters = [0] * 26
+            for words in room:
+                for c in words:
+                    letters[ord(c) - ord('a')] += 1
+
+            common = [] # most common letters in descending order
+            seenI = set()
+            seenN = set()
+            while len(common) < 5:
+                # get nth most common letter
+                n = 0
+                # ni = 0
+                for i in range(len(letters)):
+                    if i not in seenI and letters[i] not in seenN and letters[i] > n:
+                        n = letters[i]
+                        # ni = i
+                seenN.add(n)
+
+                # get all nth most common letter
+                for i in range(len(letters)):
+                    if i not in seenI and letters[i] == n:
+                        common.append(i)
+
+            isRealRoom = True
+            for i in range(5):
+                if chr(common[i]+ord('a')) not in checksum:
+                    isRealRoom = False
+                    break
+
+            if isRealRoom:
+                sumOfSectorIDs += sectorID
+
+        print("Sum of sector IDs:", sumOfSectorIDs)
+
+
 
     def solve2(self):
         print("--- Part Two ---")
