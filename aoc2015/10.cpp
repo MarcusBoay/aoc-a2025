aeaca0038882eb944df4b12c109d9cffaac08ac4
MarcusBoay <marcusboay@hotmail.com>
2025-04-15 03:23:40 -0400

    2015 10.2 cpp more perf improvements

--- aoc2015/10.cpp
+++ aoc2015/10.cpp
@@ -2,11 +2,12 @@
 #include <iostream>
 #include <string>
 #include <unordered_map>
+#include <cstdint>
 
 using namespace std;
 
 int main(void) {
-    short in = 70;
+    uint8_t in = 70;
     string elements[93] = {
         "0", // N/A
         "22", // 1
@@ -102,120 +103,122 @@ int main(void) {
         "13", // 91
         "3", // 92
     };
-    unordered_map<short, vector<short>> decaysInto = {};
-    decaysInto.insert({1, {1}});
-    decaysInto.insert({2, {72,91,1,20,3}});
-    decaysInto.insert({3, {2}});
-    decaysInto.insert({4, {32,20,3}});
-    decaysInto.insert({5, {4}});
-    decaysInto.insert({6, {5}});
-    decaysInto.insert({7, {6}});
-    decaysInto.insert({8, {7}});
-    decaysInto.insert({9, {8}});
-    decaysInto.insert({10, {9}});
-    decaysInto.insert({11, {10}});
-    decaysInto.insert({12, {61,11}});
-    decaysInto.insert({13, {12}});
-    decaysInto.insert({14, {13}});
-    decaysInto.insert({15, {67,14}});
-    decaysInto.insert({16, {15}});
-    decaysInto.insert({17, {16}});
-    decaysInto.insert({18, {17}});
-    decaysInto.insert({19, {18}});
-    decaysInto.insert({20, {19}});
-    decaysInto.insert({21, {67,91,1,20,27}});
-    decaysInto.insert({22, {21}});
-    decaysInto.insert({23, {22}});
-    decaysInto.insert({24, {23}});
-    decaysInto.insert({25, {24,14}});
-    decaysInto.insert({26, {25}});
-    decaysInto.insert({27, {26}});
-    decaysInto.insert({28, {30,27}});
-    decaysInto.insert({29, {28}});
-    decaysInto.insert({30, {29}});
-    decaysInto.insert({31, {63,20,89,1,20,30}});
-    decaysInto.insert({32, {67,31}});
-    decaysInto.insert({33, {32,11}});
-    decaysInto.insert({34, {33}});
-    decaysInto.insert({35, {34}});
-    decaysInto.insert({36, {35}});
-    decaysInto.insert({37, {36}});
-    decaysInto.insert({38, {37}});
-    decaysInto.insert({39, {38,92}});
-    decaysInto.insert({40, {39,1,20,43}});
-    decaysInto.insert({41, {68,40}});
-    decaysInto.insert({42, {41}});
-    decaysInto.insert({43, {42}});
-    decaysInto.insert({44, {63,20,43}});
-    decaysInto.insert({45, {67,44}});
-    decaysInto.insert({46, {45}});
-    decaysInto.insert({47, {46}});
-    decaysInto.insert({48, {47}});
-    decaysInto.insert({49, {48}});
-    decaysInto.insert({50, {49}});
-    decaysInto.insert({51, {61,50}});
-    decaysInto.insert({52, {63,20,51}});
-    decaysInto.insert({53, {67,52}});
-    decaysInto.insert({54, {53}});
-    decaysInto.insert({55, {54}});
-    decaysInto.insert({56, {55}});
-    decaysInto.insert({57, {56}});
-    decaysInto.insert({58, {57,1,20,27}});
-    decaysInto.insert({59, {58}});
-    decaysInto.insert({60, {59}});
-    decaysInto.insert({61, {60}});
-    decaysInto.insert({62, {61,20,30}});
-    decaysInto.insert({63, {62}});
-    decaysInto.insert({64, {63,20,27}});
-    decaysInto.insert({65, {67,64}});
-    decaysInto.insert({66, {65}});
-    decaysInto.insert({67, {66}});
-    decaysInto.insert({68, {67,61}});
-    decaysInto.insert({69, {68,20,27}});
-    decaysInto.insert({70, {69}});
-    decaysInto.insert({71, {70}});
-    decaysInto.insert({72, {71}});
-    decaysInto.insert({73, {72,91,1,20,74}});
-    decaysInto.insert({74, {73}});
-    decaysInto.insert({75, {32,20,74}});
-    decaysInto.insert({76, {75}});
-    decaysInto.insert({77, {76}});
-    decaysInto.insert({78, {77}});
-    decaysInto.insert({79, {78}});
-    decaysInto.insert({80, {79}});
-    decaysInto.insert({81, {80}});
-    decaysInto.insert({82, {81}});
-    decaysInto.insert({83, {61,82}});
-    decaysInto.insert({84, {83}});
-    decaysInto.insert({85, {84}});
-    decaysInto.insert({86, {67,85}});
-    decaysInto.insert({87, {86}});
-    decaysInto.insert({88, {87}});
-    decaysInto.insert({89, {88}});
-    decaysInto.insert({90, {89}});
-    decaysInto.insert({91, {90}});
-    decaysInto.insert({92, {91}});
+    vector<uint8_t> decaysInto[93] = {
+        {0},
+        {1},
+        {72,91,1,20,3},
+        {2},
+        {32,20,3},
+        {4},
+        {5},
+        {6},
+        {7},
+        {8},
+        {9},
+        {10},
+        {61,11},
+        {12},
+        {13},
+        {67,14},
+        {15},
+        {16},
+        {17},
+        {18},
+        {19},
+        {67,91,1,20,27},
+        {21},
+        {22},
+        {23},
+        {24,14},
+        {25},
+        {26},
+        {30,27},
+        {28},
+        {29},
+        {63,20,89,1,20,30},
+        {67,31},
+        {32,11},
+        {33},
+        {34},
+        {35},
+        {36},
+        {37},
+        {38,92},
+        {39,1,20,43},
+        {68,40},
+        {41},
+        {42},
+        {63,20,43},
+        {67,44},
+        {45},
+        {46},
+        {47},
+        {48},
+        {49},
+        {61,50},
+        {63,20,51},
+        {67,52},
+        {53},
+        {54},
+        {55},
+        {56},
+        {57,1,20,27},
+        {58},
+        {59},
+        {60},
+        {61,20,30},
+        {62},
+        {63,20,27},
+        {67,64},
+        {65},
+        {66},
+        {67,61},
+        {68,20,27},
+        {69},
+        {70},
+        {71},
+        {72,91,1,20,74},
+        {73},
+        {32,20,74},
+        {75},
+        {76},
+        {77},
+        {78},
+        {79},
+        {80},
+        {81},
+        {61,82},
+        {83},
+        {84},
+        {67,85},
+        {86},
+        {87},
+        {88},
+        {89},
+        {90},
+        {91}
+    };
 
-    queue<short> q;
+    queue<uint8_t> q;
     q.push(in);
-    short i = 0;
-    short n = 75;
+    uint8_t i = 0;
+    uint8_t n = 75;
     while (i < n) {
-        i++;
-        unsigned long long qn = q.size();
+        ++i;
+        uint64_t qn = q.size();
         if (i > 40) {
-            cout << "iteration " << i << endl;
+            cout << "iteration " << int(i) << endl;
         }
-        while (qn > 0) {
+        while (qn) {
+            --qn;
             auto cur = q.front();
             q.pop();
-            qn--;
             for (auto d : decaysInto[cur]) {
                 q.push(d);
             }
         }
     }
-    unsigned long long totalLength = 0;
+    uint64_t totalLength = 0;
     for (; !q.empty(); q.pop()) {
         totalLength += elements[q.front()].size();
     }
