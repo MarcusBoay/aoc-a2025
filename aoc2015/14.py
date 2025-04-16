15eede1d779f8461b66eb1b24f602f65cf23de54
MarcusBoay <marcusboay@hotmail.com>
2025-04-15 23:02:27 -0400

    2015 14.2

--- aoc2015/14.py
+++ aoc2015/14.py
@@ -1,16 +1,17 @@
 QUIZ_NUMBER = "14"
 
 class Solution:
-    def run(fileName = QUIZ_NUMBER + ".in"):
-        solution = Solution(fileName)
+    def run(fileName = QUIZ_NUMBER + ".in", n=2503):
+        solution = Solution(fileName, n)
         solution.solve1()
         solution.solve2()
 
     def test():
-        Solution.run(QUIZ_NUMBER + ".ex.in")
+        Solution.run(QUIZ_NUMBER + ".ex.in", 1000)
 
-    def __init__(self, fileName):
+    def __init__(self, fileName, n):
         self.ins = []
+        self.n = n
         self.fileName = fileName
         fp = open(fileName, 'r')
         arr = list(map(str, fp.read().split('\n')))
@@ -22,8 +23,6 @@ class Solution:
 
     def solve1(self):
         print("--- Part One ---")
-        n = 2503
-        # 0 4 7 14
         nameMap = dict()
         speed = [0] * len(self.ins)
         period = [0] * len(self.ins)
@@ -35,7 +34,7 @@ class Solution:
             rest[i] = int(self.ins[i][13])
         dist = [0] * len(self.ins)
         cur = period[:]
-        for i in range(n):
+        for i in range(self.n):
             for j in range(len(speed)):
                 # process rest first
                 if -cur[j] >= rest[j]:
@@ -47,12 +46,48 @@ class Solution:
                 if cur[j] > 0:
                     dist[j] += speed[j]
                     cur[j] -= 1
-        print("Distance travelled by all reindeer:", dist)
-        print("Distance travelled by winning reindeer:", max(dist))
+        print("Distance travelled by all reindeer after", self.n,"seconds:", dist)
+        print("Distance travelled by winning reindeer after", self.n,"seconds:", max(dist))
 
 
     def solve2(self):
         print("--- Part Two ---")
+        nameMap = dict()
+        speed = [0] * len(self.ins)
+        period = [0] * len(self.ins)
+        rest = [0] * len(self.ins)
+        for i in range(len(self.ins)):
+            nameMap[i] = self.ins[i][0]
+            speed[i] = int(self.ins[i][3])
+            period[i] = int(self.ins[i][6])
+            rest[i] = int(self.ins[i][13])
+        dist = [0] * len(self.ins)
+        points = [0] * len(self.ins)
+        cur = period[:]
+        for i in range(self.n):
+            for j in range(len(speed)):
+                # process rest first
+                if -cur[j] >= rest[j]:
+                    cur[j] = period[j]
+                elif cur[j] <= 0:
+                    cur[j] -= 1
+
+                # process fly
+                if cur[j] > 0:
+                    dist[j] += speed[j]
+                    cur[j] -= 1
+            curWinner = 0
+            for j in range(len(points)):
+                if dist[j] > curWinner:
+                    curWinner = dist[j]
+            ties = []
+            for j in range(len(points)):
+                if dist[j] == curWinner:
+                    ties.append(j)
+            for j in ties:
+                points[j] += 1
+        print("Points of the all reindeer after", self.n,"seconds:", points)
+        print("Points of the winning reindeer after", self.n,"seconds:", max(points))
 
 def main():
     Solution.test()
