7563ab4857a6becb7688b9f990b8c7050e95edf7
MarcusBoay <marcusboay@hotmail.com>
2025-04-17 22:53:00 -0400

    2015 19.2

--- aoc2015/19.py
+++ aoc2015/19.py
@@ -59,10 +59,10 @@ class Solution:
         for r in self.replacements:
             allR.add(r)
         for r in self.replacements:
-            for rr in self.replacements[r]:
+            for longestReplacement in self.replacements[r]:
                 cur = ""
                 allCur = set()
-                for c in rr:
+                for c in longestReplacement:
                     if c.islower():
                         cur += c
                     else:
@@ -93,7 +93,67 @@ class Solution:
             pass
         if self.startingMolecule[prevEnd::]:
             splits.append(self.startingMolecule[prevEnd::])
+        # for m in splits:
+        #     print(m)
+
+        self.reverseReplacements = dict()
+        for i in range(len(self.ins)):
+            if len(self.ins[i]) == 0:
+                break
+            line = self.ins[i].split(' => ')
+            if line[1] not in self.reverseReplacements:
+                self.reverseReplacements[line[1]] = []
+            self.reverseReplacements[line[1]].append(line[0])
+        replacements = 0
+        for i in range(len(splits)):
+            if splits[i] in self.reverseReplacements:
+                replacements += 1
+                print(f"replacing {splits[i]} for {self.reverseReplacements[splits[i]][0]}")
+                splits[i] = self.reverseReplacements[splits[i]][0]
         print(splits)
+        didReplace = True
+        while didReplace:
+            didReplace = False
+            for i in range(len(splits)):
+                s = splits[i]
+                longestReplacementN = 0
+                longestReplacement = ""
+                for r in self.reverseReplacements:
+                    if s.find(r) >= 0 and len(r) > longestReplacementN:
+                        print(r)
+                        longestReplacementN = len(r)
+                        longestReplacement = r
+                        didReplace = True
+                if didReplace:
+                    si = s.find(longestReplacement)
+                    print(longestReplacement)
+                    print(f"replacing {longestReplacement} in {splits[i]} for {self.reverseReplacements[longestReplacement][0]}")
+                    splits[i] = s[0:si] + self.reverseReplacements[longestReplacement][0] + s[si+longestReplacementN::]
+                    replacements += 1
+                    break
+                    # exit()
+        print(splits)
+        newMolecule = "".join(splits)
+        didReplace = True
+        while didReplace:
+            didReplace = False
+            # for i in range(len(splits)):
+            s = newMolecule
+            longestReplacementN = 0
+            longestReplacement = ""
+            for r in self.reverseReplacements:
+                if s.find(r) >= 0 and len(r) > longestReplacementN:
+                    print(r)
+                    longestReplacementN = len(r)
+                    longestReplacement = r
+                    didReplace = True
+            if didReplace:
+                si = s.find(longestReplacement)
+                print(longestReplacement)
+                print(f"replacing {longestReplacement} in {s} for {self.reverseReplacements[longestReplacement][0]}")
+                newMolecule = s[0:si] + self.reverseReplacements[longestReplacement][0] + s[si+longestReplacementN::]
+                replacements += 1
+        print(newMolecule, replacements)
 
 
 def main():
