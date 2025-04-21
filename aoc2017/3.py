5a2ad284e35348cd151301c07d802f193494bbd0
MarcusBoay <marcusboay@hotmail.com>
2025-04-20 21:27:57 -0400

    2017 3.1

--- aoc2017/3.py
+++ aoc2017/3.py
@@ -13,15 +13,80 @@ class Solution:
         self.ins = []
         self.fileName = fileName
         fp = open(fileName, 'r')
-        arr = list(map(str, fp.read().split('\n')))
-        for i in range(0, len(arr)):
-            line = arr[i].split('|')
-            self.ins.append(line[::])
+        self.ins = int(fp.read())
         fp.close()
         print("===", self.fileName, "===")
 
     def solve1(self):
         print("--- Part One ---")
+        l = 1
+        i = 0
+        j = 0
+        cur = 1
+        while cur < self.ins:
+            l += 2
+            j += 2
+            i += 2
+            cur += (l*4-4)
+        # go back to inner circle
+        cur -= (l*4-4)
+        i -= 2
+        j -= 2
+        l -= 2
+        # step into the outer circle
+        l += 2
+        j += 1
+        cur += 1
+        # expand the coords due to the outer circle
+        i += 1
+        j += 1
+
+        # run along the length of the outer circle
+        cl = l-2
+        print(f"currently at {cur}, coords: {i}, {j}, {cl}")
+        while cl and cur != self.ins:
+            cl -= 1
+            i -= 1
+            cur += 1
+        print(i, j)
+        if cur == self.ins:
+            self.printSteps(i,j,l)
+            return
+        cl = l-1
+        while cl and cur != self.ins:
+            cl -= 1
+            j -= 1
+            cur += 1
+        print(i, j)
+        if cur == self.ins:
+            self.printSteps(i,j,l)
+            return
+        cl = l-1
+        while cl and cur != self.ins:
+            cl -= 1
+            i += 1
+            cur += 1
+        print(i, j)
+        if cur == self.ins:
+            self.printSteps(i,j,l)
+            return
+        cl = l-1
+        while cl and cur != self.ins:
+            cl -= 1
+            j += 1
+            cur += 1
+        print(i, j)
+        if cur == self.ins:
+            self.printSteps(i,j,l)
+            return
+
+    def printSteps(self,i,j,l):
+        print(f"d.Number of steps to get to {self.ins}: {self.getSteps(i,j,l)}")
+
+    def getSteps(self,i,j,l):
+        i = abs(i - (l//2))
+        j = abs(j - (l//2))
+        return i+j
 
     def solve2(self):
         print("--- Part Two ---")
