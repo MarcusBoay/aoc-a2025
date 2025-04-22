bdbe0ef50a9e509082b87000abdb89f705ee8ee5
MarcusBoay <marcusboay@hotmail.com>
2025-04-21 20:55:40 -0400

    2016 13.2

--- aoc2016/13.py
+++ aoc2016/13.py
@@ -68,6 +68,46 @@ class Solution:
 
     def solve2(self):
         print("--- Part Two ---")
+        xn, yn = 50, 50
+        # xt, yt = 7, 4
+        xt, yt = 31, 39
+        maze = [['.' for x in range(xn)] for y in range(yn)]
+
+        # fill the maze
+        for x in range(xn):
+            for y in range(yn):
+                isWall = False
+                yoyoma = x*x + 3*x + 2*x*y + y + y*y + self.ins
+                while yoyoma:
+                    yoyoma &= yoyoma-1
+                    isWall = not isWall
+                if isWall:
+                    maze[y][x] = '#'
+
+        # bfs to get shortest path
+        q = [(1,1)] # y,x
+        seen = set()
+        steps = 51
+        while steps:
+            qn = len(q)
+            while qn:
+                qn -= 1
+                (y,x) = q.pop(0)
+                seen.add((y,x))
+                ny,nx = y,x-1
+                if nx >= 0 and (ny,nx) not in seen and maze[ny][nx] == '.':
+                    q.append((ny,nx))
+                ny,nx = y,x+1
+                if nx < xn and (ny,nx) not in seen and maze[ny][nx] == '.':
+                    q.append((ny,nx))
+                ny,nx = y-1,x
+                if ny >= 0 and (ny,nx) not in seen and maze[ny][nx] == '.':
+                    q.append((ny,nx))
+                ny,nx = y+1,x
+                if ny < yn and (ny,nx) not in seen and maze[ny][nx] == '.':
+                    q.append((ny,nx))
+            steps -= 1
+        print(f"Number of locations that can be reached in 50 steps: {len(seen)}")
 
 def main():
     # Solution.test()
