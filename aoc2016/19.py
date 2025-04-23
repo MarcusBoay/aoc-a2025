e4231e4340d4119a3de7460b8145371799e8e4e7
MarcusBoay <marcusboay@hotmail.com>
2025-04-22 22:02:27 -0400

    2016 19.2

--- aoc2016/19.py
+++ aoc2016/19.py
@@ -31,6 +31,35 @@ class Solution:
 
     def solve2(self):
         print("--- Part Two ---")
+        # for n in range(2, 210):
+        #     arr = [0] * n
+        #     for i in range(n):
+        #         arr[i] = i+1
+        #     while len(arr) > 1:
+        #         arr.pop(len(arr)//2)
+        #         prev = arr.pop(0)
+        #         arr.append(prev)
+        #     print(f"{n}: {arr[0]}")
+        # it seems be powers of 3 in this case, and after the reset, it increases by 1 until it equals the previous power of 3 then it increases by 2 until the next power of 3 where it caps at itself
+        r = self.ins
+        threeN = -1
+        while r >= 3:
+            r //= 3
+            threeN += 1
+        rem = 0
+        t = 3
+        while threeN:
+            t *= 3
+            threeN -= 1
+        rem = self.ins - t
+        # print(rem)
+        if rem <= t:
+            winningElf = rem
+        else:
+            rem -= t
+            rem *= 2
+            winningElf = t + rem
+        print(f"Elf that gets all the presents: {winningElf}")
 
 def main():
     Solution.test()
