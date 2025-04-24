7c2fb8b38be0520c4453af4db6218b5cce37ceb1
MarcusBoay <marcusboay@hotmail.com>
2025-04-23 21:18:47 -0400

    2016 22.1

--- aoc2016/21.py
+++ aoc2016/21.py
@@ -25,7 +25,7 @@ class Solution:
         print("--- Part One ---")
         pwd = list(map(str, self.password))
         for inst in self.ins:
-            # print(f"{"".join(pwd)} - {" ".join(inst)}")
+            print(f"{"".join(pwd)} - {" ".join(inst)}")
             if inst[0] == "swap":
                 if inst[1] == "position":
                     i, j = int(inst[2]), int(inst[5])
@@ -67,14 +67,79 @@ class Solution:
                 i, j = int(inst[2]), int(inst[5])
                 c = pwd.pop(i)
                 pwd.insert(j, c)
-        print(f"The result of scrabling '{self.password}': {"".join(pwd)}")
+        print(f"The result of scrambling '{self.password}': {"".join(pwd)}")
 
     def solve2(self):
         print("--- Part Two ---")
+        scrambledPwd = "decab" if self.fileName == QUIZ_NUMBER + ".ex.in" else "fbgdceah"
+        pwd = list(scrambledPwd)
+        for instI in range(len(self.ins)-1, -1, -1):
+            inst = self.ins[instI]
+            print(f"{"".join(pwd)} - {" ".join(inst)}")
+            if inst[0] == "swap":
+                if inst[1] == "position":
+                    i, j = int(inst[2]), int(inst[5])
+                    pwd[i], pwd[j] = pwd[j], pwd[i]
+                elif inst[1] == "letter":
+                    li, lj = inst[2], inst[5]
+                    i, j = 0, 0
+                    for k in range(len(pwd)):
+                        if pwd[k] == li:
+                            i = k
+                        if pwd[k] == lj:
+                            j = k
+                    pwd[i], pwd[j] = pwd[j], pwd[i]
+            elif inst[0] == "rotate":
+                if inst[1] == "based":
+                    l = inst[6]
+                    j = 0
+                    for k in range(len(pwd)):
+                        if pwd[k] == l:
+                            j = k
+                            break
+                    r = j
+                    if j == 0:
+                        r = len(pwd)
+                    # FIXME: THIS DOESN'T WORK, THERE ARE MULTIPLE SOLUTIONS
+                    # brute force searching previous index before rotation
+                    # prevI = 0
+                    # for i in range(len(pwd)-1, -1, -1):
+                    #     k = 2*i + 1
+                    #     if k%len(pwd) == j and i < 4:
+                    #         prevI = i
+                    #         print(prevI)
+                    #         break
+                    #     elif (k+1)%len(pwd) == j and i >= 4:
+                    #         prevI = i
+                    #         print(prevI)
+                    #         break
+                    # print(prevI)
+                    # r = 1 + prevI
+                    # if prevI >= 4:
+                    #     r += 1
+                    # r = len(pwd)%len(pwd)
+                    pwd = pwd[r:] + pwd[0:r]
+                else: # left/right
+                    r = int(inst[2])
+                    if inst[1] == "left":
+                        pwd = pwd[len(pwd)-r:] + pwd[0:len(pwd)-r]
+                    elif inst[1] == "right":
+                        pwd = pwd[r:] + pwd[0:r]
+            elif inst[0] == "reverse":
+                i, j = int(inst[2]), int(inst[4])
+                while i < j:
+                    pwd[i], pwd[j] = pwd[j], pwd[i]
+                    i += 1
+                    j -= 1
+            elif inst[0] == "move":
+                i, j = int(inst[2]), int(inst[5])
+                c = pwd.pop(j)
+                pwd.insert(i, c)
+        print(f"The result of un-scrambling '{scrambledPwd}': {"".join(pwd)}")
 
 def main():
     Solution.test()
-    Solution.run()
+    # Solution.run()
 
 if __name__ == "__main__":
     main()
