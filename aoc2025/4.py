d7623668025af0f7622699717f94669966aad84f
MarcusBoay <marcusboay@hotmail.com>
2025-12-04 00:13:13 -0500

    2025 4.1

---
+++ aoc2025/4.py
@@ -0,0 +1,57 @@
+QUIZ_NUMBER = "4"
+
+class Solution:
+    def run(fileName = QUIZ_NUMBER + ".in"):
+        solution = Solution(fileName)
+        solution.solve1()
+        solution.solve2()
+
+    def test():
+        Solution.run(QUIZ_NUMBER + ".ex.in")
+
+    def __init__(self, fileName):
+        self.ins = []
+        self.fileName = fileName
+        fp = open(fileName, 'r')
+        arr = list(map(str, fp.read().split('\n')))
+        for i in range(0, len(arr)-1):
+            line = list(arr[i])
+            self.ins.append(line[::])
+        fp.close()
+        print("===", self.fileName, "===")
+
+    def solve1(self):
+        print("--- Part One ---")
+        total = 0
+        for i in range(len(self.ins)):
+            for j in range(len(self.ins[i])):
+                if self.ins[i][j] == '.':
+                    continue
+                allNext = [(i-1, j), (i-1, j+1), (i,j+1), (i+1,j+1), \
+                           (i+1, j), (i+1, j-1), (i, j-1), (i-1, j-1)]
+                adjacent = 0
+                for ni, nj in allNext:
+                    if ni < 0 or ni >= len(self.ins) or \
+                        nj < 0 or nj >= len(self.ins[i]):
+                        continue
+                    # print(ni, len(self.ins), nj, len(self.ins[i]))
+                    # print(self.ins[ni])
+                    if self.ins[ni][nj] == '@' or self.ins[ni][nj] == 'x':
+                        adjacent += 1
+                if adjacent < 4:
+                    total += 1
+                    self.ins[i][j] = 'x'
+
+        # for i in range(len(self.ins)):
+        #     print("".join(self.ins[i]))
+        print(f"Number of rolls of accessible: {total}")
+
+    def solve2(self):
+        print("--- Part Two ---")
+
+def main():
+    Solution.test()
+    Solution.run()
+
+if __name__ == "__main__":
+    main()
